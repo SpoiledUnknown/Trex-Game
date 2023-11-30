6c1ad0c9ce03b3504438214f301f0af10342bde4
Spoiled Unknown <58999771+SpoiledUnknown@users.noreply.github.com>
2023-11-30 22:49:24 +0530

    Add files via upload

---
+++ BounceOff-IsTouching.js
@@ -0,0 +1,35 @@
+function isTouching(body1,body2)
+{
+    if(body1.x-body2.x<body1.width/2+body2.width/2 
+        && body2.x-body1.x<body1.width/2+body2.width/2 
+        && body2.y-body1.y<body1.height/2+body2.height/2
+        && body1.y-body2.y<body1.height/2+body2.height/2  )
+    {
+       return true;
+    
+    }
+    else{
+       return false;
+    }
+}
+
+function bounceOff(body1,body2)
+{
+    if(body1.x-body2.x<body1.width/2+body2.width/2 
+        && body2.x-body1.x<body1.width/2+body2.width/2 )
+        {
+              body1.velocityX=body1.velocityX*(-1);
+              body2.velocityX=body2.velocityX*(-1);
+       
+        }
+        
+        
+        if(body2.y-body1.y<body1.height/2+body2.height/2
+        && body1.y-body2.y<body1.height/2+body2.height/2  )
+    {
+              body1.velocityY=body1.velocityY*(-1);
+              body2.velocityY=body2.velocityY*(-1);
+    
+    }
+   
+}
